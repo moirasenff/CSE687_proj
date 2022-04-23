0991470196db10c48c989e5292b6e95b8f0cc501
moirasenff <103228001+moirasenff@users.noreply.github.com>
2022-04-23 12:06:56 -0400

    FileManager hpp and cpp update
    Added fetchLines and write functions to FileManager class

--- Phase 1/repo/fileManager.cpp
+++ Phase 1/repo/fileManager.cpp
@@ -1,6 +1,25 @@
 #include "FileManager.hpp"
 #include <fstream>
+#include <filesystem>
 #include <boost/log/trivial.hpp>
+
+std::vector<std::string> FileManager::fetchLines(std::string input) {
+	std::vector<std::string> linesVector;
+	std::string content;
+	std::string directoryPath = input; //store input directory in directoryPath variable
+
+	for (const auto & entry : std::filesystem::directory_iterator(directoryPath)) { //for each file in the directory
+		std::ifstream file;
+		file.open(directoryPath); //open the file
+
+		while (file.good()) {
+			file >> content; //push into content variable
+			linesVector.push_back(content); //push onto the vector
+		}
+	}
+
+	return linesVector; //return vector
+}
 
 std::vector<std::string> FileManager::read(std::string directory, int key) {
 	std::vector<std::string> retVect;
@@ -16,4 +35,16 @@ std::vector<std::string> FileManager::read(std::string directory, int key) {
 		retVect.push_back(line);
 	}
 	return retVect;
+}
+
+void FileManager::write(std::string directory, int key, std::string content) {
+	std::string filePath = directory; //store desired directory input into path variable
+	filePath = "/file" + std::to_string(key) + ".txt"; //update path with file named based on specific key of file
+
+	BOOST_LOG_TRIVIAL(info) << "file path: " << filePath;
+
+	std::ofstream file(filePath); //specifies directory to be opened in
+	file.open("file" + std::to_string(key) + ".txt", std::ios::app); //open file
+	file << content; //write content into file
+	file.close(); //close file
 }
--- Phase 1/repo/fileManager.hpp
+++ Phase 1/repo/fileManager.hpp
@@ -5,5 +5,7 @@
 
 class FileManager {
 public:
+	std::vector<std::string> fetchLines(std::string input);
 	std::vector<std::string> read(std::string directory, int key = 0);
+	void write(std::string directory, int key, std::string content);
 };
